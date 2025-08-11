8a622cf9f85c1c9675c2f8ec573ebd7c471db3c0
Olafur Arason <olafura@olafura.com>
2025-08-11 13:17:11 +0200

    chore: add gitignore and profile data

---
+++ profile-data/get_big.sh
@@ -0,0 +1,2 @@
+curl -o profile-data/ModernAtomic.json https://mtgjson.com/api/v5/ModernAtomic.json
+curl -o profile-data/LegacyAtomic.json https://mtgjson.com/api/v5/LegacyAtomic.json
---
+++ profile-data/get_medium.sh
@@ -0,0 +1,2 @@
+curl -o profile-data/cdc.json https://data.cdc.gov/api/views/25m4-6qqq/rows.json?accessType=DOWNLOAD
+curl -o profile-data/edg.json https://edg.epa.gov/data.json
